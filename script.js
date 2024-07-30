1a88c841154171116cb6f025845358b75bed4fa8
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-07-31 00:02:15 +0300

    step 28

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ window.onload = () => {
             input.type = "text";
             input.id = letter + number;
             input.ariaLabel = letter + number;
+            input.onchange = update;
             container.appendChild(input);
         });
     });
