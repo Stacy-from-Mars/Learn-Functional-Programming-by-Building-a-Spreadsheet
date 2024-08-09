e7d830a9bfefcf3d2712c135357f4553f697696a
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-08-09 23:08:27 +0300

    step 87

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const applyFunction = (str) => {
     const str2 = infixEval(noHigh, infix);
     const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
     const toNumberList = (args) => args.split(",").map(parseFloat);
-    const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()]
+    const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
 }
 
 const evalFormula = (x, cells) => {
