eed9980fcdab4c4c27cc778367946c584acd23ed
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-08-12 00:16:31 +0300

    step 104

--- script.js
+++ script.js
@@ -39,7 +39,8 @@ const spreadsheetFunctions = {
     has2: nums => nums.includes(2),
     increment: nums => nums.map(num => num + 1),
     random: ([x, y]) => Math.floor(Math.random() * y + x),
-    range: nums => range(...nums)
+    range: nums => range(...nums),
+    nodupes: nums => [...new Set(nums).values()]
 }
 
 const applyFunction = (str) => {
