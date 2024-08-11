707e89155348eac02776bf509a92a627d0ca4a61
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-08-11 23:41:37 +0300

    step 99

--- script.js
+++ script.js
@@ -33,7 +33,9 @@ const spreadsheetFunctions = {
     median,
     even: nums => nums.filter(isEven),
     firsttwo: nums => nums.slice(0, 2),
-    lasttwo: nums => nums.slice(-2)
+    lasttwo: nums => nums.slice(-2),
+    has2: nums => nums.includes(2),
+    increment: nums => nums.map(num => num + 1)
 }
 
 const applyFunction = (str) => {
